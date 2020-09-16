1bdf81f59802443954b58e63dec83331f4a45ff6
Maria Angélica de Matos <maria.angelicamatos@gmail.com>
2020-09-16 18:25:31 +0000

    Lógica, condicionais e getElementByID

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// se os campos de email e nome tiverem preenchidos
+  // Envie os dados
+// Senão 
+  //Peça a usuária que preencha os campos
+
+  if(document.getElementByID("nome") && document.getElementByID("email")){
+    
+  } else {
+
+  }
