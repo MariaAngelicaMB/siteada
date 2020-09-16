15f283a661f7c54e6cc8cc90118ade7bd8645a29
Maria Angélica de Matos <maria.angelicamatos@gmail.com>
2020-09-16 18:48:42 +0000

    String, Operadores relacionais e Alert

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 // Senão 
   //Peça a usuária que preencha os campos
 
-  if(document.getElementByID("nome") && document.getElementByID("email")){
-    
+  if(document.getElementByID("nome").value != "" && document.getElementByID("email").value != ""){
+    alert("Prontinho! Você receberá as novidades por email.")
   } else {
-
+    alert("Por favor, preencha os campos nome e email.")
   }
